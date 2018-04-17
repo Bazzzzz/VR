236c12cc56ff8afa22983b861a26957df7bd0709
Bazzzzz <sebastiaan.koch@hotmail.com>
2018-04-17 22:34:46 +0200

    Animations gobllin MainScene modeling/rigidbodies complete

---
+++ VIP/Assets/GoblinController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinController : MonoBehaviour {
+    public Animator anim;
+    private int isDying = Animator.StringToHash("anim_dying");
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(isDying);
+        }
+	}
+}
---
+++ VIP/Assets/Scripts/BallAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnimation : MonoBehaviour {
+    public Texture[] textures;
+
+    public float changeInterval = 0.33F;
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (textures.Length == 0)
+        {
+            return;
+        }
+
+        int index = Mathf.FloorToInt(Time.time / changeInterval);
+        index = index % textures.Length;
+        
+        rend.materials[1].mainTexture = textures[index];
+    }
+}
